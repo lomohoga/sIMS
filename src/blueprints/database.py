d85173f62fb8dd654f0171d770728357224df2bf
Braullo Jo <bajo@up.edu.ph>
2023-07-01 01:50:11 +0800

    Disable ONLY_FULL_GROUP_BY every session

--- src/blueprints/database.py
+++ src/blueprints/database.py
@@ -4,7 +4,7 @@ from src.blueprints.exceptions import DatabaseConnectionError
 
 def connect_db ():
     try:
-        connection = mysql.connector.connect(host = "localhost", user = "root", password = "password", database = "sims")
+        connection = mysql.connector.connect(host = "localhost", user = "root", password = "password", database = "sims", sql_mode = "STRICT_ALL_TABLES,NO_ENGINE_SUBSTITUTION")
     except Exception as e:
         raise DatabaseConnectionError 
 
